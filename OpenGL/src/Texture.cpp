333b97499fad27b458af4d482209f3e14557ce49
hebohang <hebohang1113@gmail.com>
2021-12-29 19:33:18 +0800

    20_Projection Matrices in OpenGL

--- OpenGL/src/Texture.cpp
+++ OpenGL/src/Texture.cpp
@@ -1,6 +1,6 @@
 #include "Texture.h"
 
-#include "vendor/stb_image/stb_image.h"
+#include "stb_image/stb_image.h"
 
 Texture::Texture(const std::string& path)
 	: m_RendererID(0), m_FilePath(path), m_LocalBuffer(nullptr), 
